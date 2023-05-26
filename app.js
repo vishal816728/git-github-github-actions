fa498307d7c8305aa19f49295d54579cdbfd8ed8
unknown <vk8167282@gmail.com>
2023-05-26 17:50:27 +0530

    git-1.0.3

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+console.log(
+  "Head-> main ,,,, main is the branch and head refers that content is currently visible from which branch "
+);
+
 function middle(req, res, next) {
   console.log("middle ware is working in between");
   next();
@@ -15,6 +19,7 @@ app.get("/get", middle, (req, res) => {
 app.get("/new_branch", (req, res) => {
   res.send("new branch is coming");
 });
+console.log("staging area is moved");
 
 app.get("/", (req, res) => {
   res.send("home");
