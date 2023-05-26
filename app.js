8a2b3b110dcc8ca9b6100e32038a9442848cf855
unknown <vk8167282@gmail.com>
2023-05-26 17:19:55 +0530

    git-1.0.1

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.get("/get", (req, res) => {
+function middle(req, res, next) {
+  console.log("middle ware is working in between");
+  next();
+}
+app.use((req, res, next) => {
+  console.log("middleware 2");
+  next();
+});
+app.get("/get", middle, (req, res) => {
   res.send("Don't watch you are not going to take it");
+});
+
+app.get("/", (req, res) => {
+  res.send("home");
 });
 
 app.listen(3000, () => {
